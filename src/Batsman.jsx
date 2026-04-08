96c3497d38c8592a895e17a8da80d6f44919b388
Md Rayhan ul Fardous <49313477+rayhan-fardous@users.noreply.github.com>
2026-04-08 10:12:04 +0600

    Batsman added

--- src/Batsman.jsx
+++ src/Batsman.jsx
@@ -2,19 +2,34 @@ import { useState } from "react";
 
 export default function Batsman() {
   const [runs, setRuns] = useState(0);
+  const [sixes, setSixes] = useState(0);
+
   const handleSingle = () => {
-        const updatedRuns = runs + 1;
-        setRuns(updatedRuns);
-    }
+    const updatedRuns = runs + 1;
+    setRuns(updatedRuns);
+  };
+
+  const handleFour = () => {
+    const updatedRuns = runs + 4;
+    setRuns(updatedRuns);
+  }
+
+  const handleSix = () => {
+    const updatedRuns = runs + 6;
+    const updatedSixes = sixes + 1;
+    setSixes(updatedSixes);
+    setRuns(updatedRuns);
+  };
 
   return (
     <div>
       <h3>Player: Bangla Batsman</h3>
-      
+      <p>Sixes: {sixes}</p>
       {runs > 50 && <p>You score: 50</p>}
       <h1>Score: {runs}</h1>
       <button onClick={handleSingle}>singles</button>
-      <button>Four</button>
+      <button onClick={handleFour}>Four</button>
+      <button onClick={handleSix}>Six</button>
     </div>
   );
 }
