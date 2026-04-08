0f6624c467fa09a55dac753ff599269ba87a116b
Md Rayhan ul Fardous <49313477+rayhan-fardous@users.noreply.github.com>
2026-04-08 13:23:57 +0600

    User added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import { Suspense } from 'react'
 import './App.css'
 import Batsman from './Batsman'
 import Counter from './Counter'
+import Users from './Users'
+
+const fetchUsers = fetch('https://jsonplaceholder.typicode.com/users')
+  .then(res => res.json())
+
 
 function App() {
 
 
+
   return (
     <>
+      <Suspense fallback={<h3>Loading...</h3>}>
+        <Users fetchUsers ={fetchUsers}></Users>
+      </Suspense>
       <Batsman></Batsman>
       <Counter></Counter>
     </>
